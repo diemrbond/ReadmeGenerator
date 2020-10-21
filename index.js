2d5f576d3461995965538c5b4c2f5ebbe163b072
Andrew <andrew@cell-media.com>
2020-10-21 08:13:12 +0800

    Updated questions to include better instructions

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const questions = [
         type: "input",
         message: "To get started, please enter your NAME",
         name: "name",
-        suffix: " *",
+        suffix: " *\n ",
         validate: value => value != "" ? true : logSymbols.warning + " Please enter your NAME!"
     },
     // GITHUB USERNAME
@@ -55,23 +55,27 @@ const questions = [
         type: "input",
         message: "Please enter your GITHUB USERNAME",
         name: "github",
-        suffix: " *",
+        suffix: " *\n ",
         validate: async (value) => {
 
+            // Adds a loading animation while checking github
             const throbber = ora('Checking Github...').start();
 
+            // API to search Github for the entered github name
             const queryUrl = `https://api.github.com/search/users?q=${value}`;
 
+            // Call using Axios to Github
             const axiosResults = await axios.get(queryUrl).then(function (res) {
-                
+
                 // Check there is at least 1 user that matches
                 let totalResults = res.data.total_count;
                 let loginName;
-                
-                if (totalResults > 0){
+
+                // Check there is at least 1 result
+                if (totalResults > 0) {
                     loginName = res.data.items[0].login;
                 }
-                
+
                 // If the user login is an exact match
                 if ((totalResults > 0) && (loginName === value)) {
 
@@ -86,8 +90,10 @@ const questions = [
                 // Return error back to promise
                 return logSymbols.warning + " Please enter a valid GITHUB USERNAME!";
             });
-            
+
+            // Stop the loading animation
             throbber.stop();
+
             // Return result back to inquirer
             return axiosResults;
         }
@@ -97,7 +103,8 @@ const questions = [
         type: "input",
         message: "What is your EMAIL ADDRESS?",
         name: "email",
-        suffix: " *",
+        suffix: " *\n ",
+        // Using the email-validator to check the email address
         validate: value => validator.validate(value) ? true : logSymbols.warning + " Please enter a valid EMAIL ADDRESS!"
     },
     // PROJECT TITLE
@@ -105,49 +112,55 @@ const questions = [
         type: "input",
         message: "What is the TITLE of your project?",
         name: "title",
-        suffix: " *",
+        suffix: " *\n ",
+        // Check the title isn't empty
         validate: value => value != "" ? true : logSymbols.warning + " Please enter your project TITLE!"
     },
     // DESCRIPTION
     {
         type: "input",
-        message: "What is the DESCRIPTION of your project?",
+        message: "What is the DESCRIPTION of your project?\n ",
         name: "description"
     },
     // INSTALLATION INSTRUCTIONS
     {
         type: "input",
-        message: "What are the INSTALLATION INSTRUCTIONS for your project?",
+        message: "What are the INSTALLATION INSTRUCTIONS for your project?\n ",
+        suffix: " Provide a step-by-step description of how to get the development environment running.\n ",
         name: "Installation"
     },
     // USAGE INFORMATION
     {
         type: "input",
-        message: "What is the USAGE INFORMATION for your project?",
+        message: "What is the USAGE INFORMATION for your project?\n ",
+        suffix: " Provide instructions and examples for use.\n ",
         name: "usage"
     },
     // CONTRIBUTION GUIDELINES
     {
         type: "input",
-        message: "What are the CONTRIBUTION GUIDELINES for your project?",
+        message: "What are the CONTRIBUTION GUIDELINES for your project?\n ",
+        suffix: " If you created an application or package and would like other developers to contribute it, what are your requirements?\n ",
         name: "contribution"
     },
     // COLLABORATORS
     {
         type: "input",
-        message: "Who are the COLLABORATORS for your project?",
+        message: "Who are the COLLABORATORS for your project?\n ",
+        suffix: " Give a shoutout to anyone who helped you with this project\n ",
         name: "collaborators"
     },
     // TEST INSTRUCTIONS
     {
         type: "input",
-        message: "What are the TEST INSTRUCTIONS for your project?",
+        message: "What are the TEST INSTRUCTIONS for your project?\n ",
+        suffix: " Provide examples on how to run any associated testing functionality\n ",
         name: "test"
     },
     // SCREENSHOT
     {
         type: "confirm",
-        message: "Do you have a SCREENSHOT for your project?",
+        message: "Do you have a SCREENSHOT for your project?\n ",
         name: "screenshot",
         default: true
     },
@@ -156,22 +169,24 @@ const questions = [
         type: "input",
         message: "What is the URL of your SCREENSHOT?",
         name: "screenshot_url",
-        suffix: " *",
+        suffix: " *\n You can use either absolute http://full/path/to/img.jpg or relative /relative/path/to/img.jpg\n ",
         when: (answers) => answers.screenshot === true,
         validate: value => value != "" ? true : logSymbols.warning + " Please enter a SCREENSHOT URL!"
     },
     // LICENSE
     {
         type: "list",
-        message: "Please select a LICENSE for this project?",
+        message: "Please select a LICENSE for this project?\n ",
         name: "license",
+        suffix: " For more information, visit https://choosealicense.com/\n ",
         choices: ["GNU GPLv3", "MIT License", "ISC License", "Apache License 2.0", "No license"]
     },
     // LICENSE CHECK
     {
         type: "confirm",
-        message: "Are you sure you don't want to include a LICENSE?",
+        message: "Are you sure you don't want to include a LICENSE?\n ",
         name: "license_confirm",
+        suffix: " We really would recommend it.\n ",
         default: false,
         when: (answers) => answers.license === "No license"
     },
@@ -180,7 +195,7 @@ const questions = [
         type: "list",
         message: "Please select a LICENSE for this project?",
         name: "license_retry",
-        suffix: " *",
+        suffix: " *\n For more information, visit https://choosealicense.com/\n ",
         choices: ["GNU GPLv3", "MIT License", "ISC License", "Apache License 2.0"],
         when: (answers) => answers.license_confirm === false
     }
@@ -209,7 +224,7 @@ function init() {
         .prompt(questions)
         .then(function (response) {
 
-            console.log('\n\nCreating your README.md file...')
+            console.log('\nCreating your README.md file...')
             // fs.writeFile(response.username + ".txt", JSON.stringify(response), function (err) {
             //     if (err) {
             //         console.log("Error: " + err);
